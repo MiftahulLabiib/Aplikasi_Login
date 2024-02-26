2f8413228a1fa61719c8425d11fa46292cc06493
Miftahul Labiib Syam <miftahullabiibsyam@gmail.com>
2024-02-26 22:00:44 +0800

    Rename min_absolute to min_gb, use parameter names

--- all_checks.py
+++ all_checks.py
@@ -3,13 +3,14 @@
 import os
 import sys
 
+(...)
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if disk_full():
+    if check_disk_full(disk = "/", min_gb = 2, min_percent = 10):
 	print("Disk Full."
 	sys.exit(1)
     print("EVerything Ok")
     sys.exit(0)
-main()
+(...)
